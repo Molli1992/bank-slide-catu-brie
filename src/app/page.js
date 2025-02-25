031149ff1ebfc3bae941b2204f64ebef25a48ece
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-02-25 16:13:19 -0300

    background img

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
 import styles from "./page.module.css";
+import Img1 from "../../public/img-1.jpg";
+import Img2 from "../../public/img-2.jpg";
 
 export default function Home() {
   return (
     <div className={styles.body}>
-      <h1>Mia y tomi</h1>
+      <div
+        className={styles.imgDesktop}
+        style={{
+          backgroundImage: `url("${Img2.src}")`,
+        }}
+      />
+
+      <div
+        className={styles.imgMobile}
+        style={{
+          backgroundImage: `url("${Img1.src}")`,
+        }}
+      />
     </div>
   );
 }
