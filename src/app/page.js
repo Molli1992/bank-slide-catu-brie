1da12d6e3ea685db618fa77ebf0ba73273005c30
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-03-07 10:11:21 -0300

    imagen con mejor calidad

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
-import Img1 from "../../public/img-1.jpg";
-import Img2 from "../../public/img-2.jpg";
+import Img1 from "../../public/MyT-01.jpg";
+import Img2 from "../../public/MyT-02.jpg";
 
 export default function Home() {
   return (
@@ -8,14 +8,14 @@ export default function Home() {
       <div
         className={styles.imgDesktop}
         style={{
-          backgroundImage: `url("${Img2.src}")`,
+          backgroundImage: `url("${Img1.src}")`,
         }}
       />
 
       <div
         className={styles.imgMobile}
         style={{
-          backgroundImage: `url("${Img1.src}")`,
+          backgroundImage: `url("${Img2.src}")`,
         }}
       />
     </div>
