abe3a49567575ffc856e23b47506353384d21972
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-03-07 10:47:37 -0300

    copiar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,31 @@
+"use client";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 import Img1 from "../../public/MyT-01.jpg";
 import Img2 from "../../public/MyT-02.jpg";
+import { FaRegCopy } from "react-icons/fa";
+import { CiCircleCheck } from "react-icons/ci";
 
 export default function Home() {
+  const [toast, setToast] = useState(false);
+  const [toastData, setToastData] = useState({ title: "", description: "" });
+
+  const handleCopy = (text, type) => {
+    navigator.clipboard.writeText(text);
+    setToast(true);
+    setToastData({
+      title: "Copiado",
+      description:
+        type === "CBU"
+          ? "CBU copiado correctamente"
+          : "Dirección copiada correctamente",
+    });
+
+    setTimeout(() => {
+      setToast(false);
+    }, "2500");
+  };
+
   return (
     <div className={styles.body}>
       <div
@@ -10,14 +33,75 @@ export default function Home() {
         style={{
           backgroundImage: `url("${Img1.src}")`,
         }}
-      />
+      >
+        <button
+          className={styles.copyButton}
+          onClick={() => handleCopy("2850467840095904560598", "CBU")}
+          style={{
+            top: "45%",
+            left: "74.1%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          Copiar CBU <FaRegCopy className={styles.icon} />
+        </button>
+
+        <button
+          className={styles.copyButton}
+          onClick={() => handleCopy("2850467840095904560598", "USDT")}
+          style={{
+            top: "68%",
+            left: "74.1%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          Copiar dirección de deposito <FaRegCopy className={styles.icon} />
+        </button>
+      </div>
 
       <div
         className={styles.imgMobile}
         style={{
           backgroundImage: `url("${Img2.src}")`,
         }}
-      />
+      >
+        <button
+          className={styles.copyButton}
+          onClick={() => handleCopy("2850467840095904560598", "CBU")}
+          style={{
+            top: "48%",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          Copiar CBU <FaRegCopy className={styles.icon} />
+        </button>
+
+        <button
+          className={styles.copyButton}
+          onClick={() => handleCopy("2850467840095904560598", "USDT")}
+          style={{
+            top: "69%",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          Copiar dirección de deposito <FaRegCopy className={styles.icon} />
+        </button>
+      </div>
+
+      {toast ? (
+        <div className={`${styles.bodyToast} ${styles.fadeInUp}`}>
+          <div className={styles.container}>
+            <div className={styles.containerTitle}>
+              <CiCircleCheck className={styles.checkIcon} />
+              <h1 className={styles.title}>{toastData.title}</h1>
+            </div>
+          </div>
+
+          <p className={styles.description}>{toastData.description}</p>
+        </div>
+      ) : null}
     </div>
   );
 }
